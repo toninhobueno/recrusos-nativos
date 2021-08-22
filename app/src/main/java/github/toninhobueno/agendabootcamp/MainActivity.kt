0250c7ac9f14c9c74c2ef96c297918c75dd936ca
tonin <antonio.leite00@hotmail.com>
2021-08-22 14:40:30 -0300

    Criado intent para o Calendar

--- app/src/main/java/github/toninhobueno/agendabootcamp/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/agendabootcamp/MainActivity.kt
@@ -1,7 +1,10 @@
 package github.toninhobueno.agendabootcamp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +15,14 @@ class MainActivity : AppCompatActivity() {
 
         val btnSetEvent = findViewById<Button>(R.id.set_event)
         btnSetEvent.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bootcamp")
+                .putExtra(EVENT_LOCATION, "Online")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME,System.currentTimeMillis() + (60*60*1000))
 
+            startActivity(intent)
         }
 
     }
