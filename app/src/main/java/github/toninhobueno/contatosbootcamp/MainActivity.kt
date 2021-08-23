15b5488e1d53d5882a69e4996c619889268e758f
tonin <antonio.leite00@hotmail.com>
2021-08-23 18:11:27 -0300

    Verificado permissões de usuário e feito set de contatos

--- app/src/main/java/github/toninhobueno/contatosbootcamp/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/contatosbootcamp/MainActivity.kt
@@ -1,11 +1,58 @@
 package github.toninhobueno.contatosbootcamp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.core.app.ActivityCompat
+import java.util.jar.Attributes
+
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CONTACT = 1
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS),
+                REQUEST_CONTACT)
+        } else{
+            setContacts()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CONTACT) setContacts()
+
+    }
+
+    private fun setContacts() {
+        val contactList : ArrayList<Contact> = ArrayList()
+
+        val cursor = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+
+
+        if (cursor != null){
+            while (cursor.moveToNext()){
+                contactList.add(Contact(
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)),
+                    cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                ))
+
+            }
+            cursor.close()
+        }
     }
 }
