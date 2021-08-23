837c21bba3c6922330d640c66d81ade6540a04fd
tonin <antonio.leite00@hotmail.com>
2021-08-23 17:36:58 -0300

    Criando itens para recycler e Adapters

---
+++ app/src/main/java/github/toninhobueno/contatosbootcamp/Contact.kt
@@ -0,0 +1,4 @@
+package github.toninhobueno.contatosbootcamp
+
+data class Contact (val name : String, val phoneNumber: String)
+
---
+++ app/src/main/java/github/toninhobueno/contatosbootcamp/ContactAdapter.kt
@@ -0,0 +1,42 @@
+package github.toninhobueno.contatosbootcamp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter (val contactsList : ArrayList<Contact>) : RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
+        val view =LayoutInflater.from(parent.context).inflate(R.layout.contact_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+        holder.bindItem(contactsList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return contactsList.size
+    }
+
+
+
+    class ViewHolder (itemView: View)  : RecyclerView.ViewHolder(itemView)  {
+
+        fun bindItem (contact:Contact) {
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.contact_number)
+
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+
+        }
+    }
+}
+
+
