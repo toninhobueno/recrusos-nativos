3e6b460a3d6184521511981ba54972e13bbe76c8
tonin <antonio.leite00@hotmail.com>
2021-08-24 18:50:04 -0300

    Setando permissões de usuário

--- app/src/main/java/github/toninhobueno/fotosbootcamp/MainActivity.kt
+++ app/src/main/java/github/toninhobueno/fotosbootcamp/MainActivity.kt
@@ -1,11 +1,61 @@
 package github.toninhobueno.fotosbootcamp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        pick_button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED){
+                    val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    requestPermissions(permission,PERMISSION_CODE)
+                }
+
+            }
+            else {
+                pickImageFromGalery()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGalery()
+                }else{
+                    Toast.makeText(this,"Permissão Negada",Toast.LENGTH_LONG).show()
+                }
+
+            }
+        }
+
+
+    }
+
+    private fun pickImageFromGalery() {
+
+    }
+
+    companion object{
+        private val PERMISSION_CODE = 1000
+        private val IMAGE_PICK_CODE = 1001
     }
 }
